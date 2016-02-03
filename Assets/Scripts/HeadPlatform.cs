b5c6f4ede6e80663980531a3d6d9c251960c03ef
jinu2ID <jinu.jacob90@gmail.com>
2016-02-02 19:53:09 -0500

    initial commit

---
+++ Assets/Scripts/HeadPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadPlatform : MonoBehaviour {
+
+	public GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (BodySourceView.isBodyTracked ()) {
+			//more magic
+			platform.transform.position = new Vector3 ((BodySourceView.getX () / 10)-15, -11, 0);
+		}
+	}
+}
---
+++ Assets/Sprites/ButtonWall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonWall : MonoBehaviour {
+
+	private BoxCollider2D collision;
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer> ().material.color = Color.grey;
+		collision = gameObject.GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ButtonBox.isInBox ()) {
+			if (gameObject.GetComponent<Renderer> ().material.color == Color.grey) {
+				gameObject.GetComponent<Renderer> ().material.color = Color.green;
+				collision.isTrigger = true;
+			}
+		}
+	}
+}
